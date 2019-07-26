28cccd21a909783ea4c4e0e86df88d5c7eebb12e
pwiewiora <p.wiewiora@gmail.com>
2019-07-26 13:04:45 +0200

    Added 26_creating_elements

---
+++ 26_creating_elements/app.js
@@ -0,0 +1,31 @@
+// Create element
+
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+
+// Add id
+li.id = 'new-item';
+
+// Add attribute
+li.setAttribute('title', 'New item');
+
+// Create text node and append
+li.appendChild(document.createTextNode('Appended list item'));
+
+// Create new link element
+const link = document.createElement('a');
+// Add classes
+link.className = 'delete-item secondary-content';
+// Add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+// Append link to li
+li.appendChild(link);
+
+
+// Append li as child to ul
+
+document.querySelector('ul.collection').appendChild(li);
+
+console.log(li);
