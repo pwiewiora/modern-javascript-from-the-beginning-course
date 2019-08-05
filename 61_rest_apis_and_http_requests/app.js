f83aedab52fa9282b9abb5859f2d1ecd2a3e55b5
pwiewiora <p.wiewiora@gmail.com>
2019-08-05 13:59:34 +0200

    Added and completed notes for 61_rest_apis_and_http_requests

---
+++ 61_rest_apis_and_http_requests/app.js
@@ -0,0 +1,28 @@
+/*
+WHAT IS AN API
+
+Application Programming Interface
+
+Contract provided by one piece of software to another
+Structured request and response
+We just worked with an API that takes a request and responds with jokes
+
+REST APIS
+
+Representational State Transfer
+Architecture style for designing networked applications
+Relies on a stateless, client-server protocolo, almost always HTTP
+Treats server objects as resources that can be created or destroyed
+Can be used by virtually any programming language
+All APIS have their own rules and structure
+
+TYPES OF REQUESTS
+
+GET: Retreive data from a specified resource
+POST: Submit data to be processed to a specified resource
+PUT: Update a specified resource
+DELETE: Delete a specified resource
+
+HEAD: Same as get but does not return a body
+OPTIONS: Returns the supported HTTP methods
+PATCH: Update partial resources
