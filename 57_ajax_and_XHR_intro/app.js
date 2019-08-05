bbf699559c095792fc174ed060d7993b75ce8030
pwiewiora <p.wiewiora@gmail.com>
2019-08-05 09:08:49 +0200

    Added and completed notes for 57_ajax_and_XHR_intro

---
+++ 57_ajax_and_XHR_intro/app.js
@@ -0,0 +1,28 @@
+/* What is Ajax?
+
+Asynchronous JavaScript & XML
+Set of web technologies
+Send & receive data asynchronously
+Does not interfere with the current page
+JSON has replaced XML for the most part 
+
+Make async requests in the background
+No page reload/refresh
+Fetch data
+Very interactive
+
+XmlHttpRequest (XHR) object
+
+API in the form of an object
+Provided by the browsers JS environment
+Methods transfer data between client / server
+Can be used with other protocols than HTTP
+Can work with data other than XML (JSON, plain text)
+
+Libraries & other methods
+
+Fetch API (recommended as part of core vanilla JS and nice)
+Axios
+Superagent
+jQuery (not suggested for ajax - bloated whole dom manipulation library - not necessary)
+Node HTTP
